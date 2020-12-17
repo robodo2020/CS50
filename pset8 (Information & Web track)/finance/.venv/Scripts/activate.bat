38602b798a3b5604b75e43d490382d77ca92b05c
declan <yuchun.f519@gmail.com>
2020-12-17 16:20:44 +0800

    update finance

--- pset8 (Information & Web track)/finance/.venv/Scripts/activate.bat
+++ pset8 (Information & Web track)/finance/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\demix\OneDrive\文件\Workspace\CS50\pset8\finance\.venv
+set VIRTUAL_ENV=C:\Users\demix\OneDrive\文件\Workspace\CS50\pset8 (Information & Web track)\finance\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
